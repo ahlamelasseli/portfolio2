d85bef6e7ab1a730f04e9c6594d1741398715f54
ahlam <ahlamelasseli>
2025-05-30 13:45:23 +0100

    push project

--- src/constant/image.jsx
+++ src/constant/image.jsx
@@ -1,7 +1,7 @@
 import { ImOffice } from "react-icons/im"
 import logo from "../assets/images/ahlam.png"
 import easyfolio from "../assets/images/EasyFolio-img.webp"
-import quickstart  from "../assets/images/Quickstart.webp"
+import quickstart  from "../assets/images/QuickStart.webp"
 import restaurantly from "../assets/images/Restaurantly.png"
 import library from "../assets/images/library.png"
 import machyana from "../assets/images/machy_ana.jpg"
